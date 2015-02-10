5cbb29233ea60b11e8ba35740361dc87f1358a4e
wcwz020140 <wcwz020140@163.com>
2015-02-10 23:32:22 +0800

    后台界面...

--- public/javascript/adminIndex/adminIndex.main.js
+++ public/javascript/adminIndex/adminIndex.main.js
@@ -1,9 +1,24 @@
 require.config({
 	paths: {
 		'jquery' : '../jquery-2.1.1',
+		'floder' : 'leftBarFloder'
 	}
 });
 
-require(['jquery'], function ($) {
-	alert('!')
+require(['jquery', 'floder'], function ($, f) {
+	$($('.left-bar')[0]).click(function (event) {
+		
+		switch (event.target.id) {
+			case 'site':
+				f.floder('site');
+				break;
+			case 'artical':
+				f.floder('artical');
+				break;
+			case 'img':
+				f.floder('img');
+				break;
+			default: 'What ???';
+		}
+	})
 })
