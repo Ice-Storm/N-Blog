ff3dd285c5ec4387f41a272454fd51c5cc31ba4c
wcwz020140 <wcwz020140@163.com>
2015-02-16 16:21:34 +0800

    upload have computed

--- control/admin.addArt.js
+++ control/admin.addArt.js
@@ -1,3 +1,13 @@
+var db = require('../db/db.js');
+
+var path = require('path');
+
+var fs = require('fs');
+
+var URL = require('url');
+
+var formidable = require('formidable');
+
 var dealGet = function (req, res) {
 	res.render('adminAddArt', {
 		css: 'adminIndex.adminAddArt'
@@ -5,7 +15,36 @@ var dealGet = function (req, res) {
 }
 
 var dealPost = function (req, res) {
-	console.log(req)
+	var form = new formidable.IncomingForm();
+
+	form.keepExtensions = true;
+
+	var imgName = new Date().getTime();
+
+	form.maxFieldsSize = 2 * 1024 * 1024;   
+
+	var uploadPath = path.join(__dirname, '../public/uploadImg/');
+
+	form.uploadDir = uploadPath;
+
+	form.parse(req, function(err, fields, files) {
+		
+		var extname = '';
+
+		switch (files.imgFile.type) {
+			case 'image/jpeg':
+				extname = 'jpg';
+				break;
+			case 'image/png':
+				extname = 'png';
+				break;
+		}
+		console.log(URL.parse(req.url))
+		fs.rename(files.imgFile.path, uploadPath + imgName + '.' + extname, function (err) {
+			res.json({error: 0, message: 'ok', url: '/uploadImg/' + imgName + '.' + extname})
+		})
+
+	})
 }
 
 module.exports.dealGet = dealGet;
